e2e6906e07d064375fa8904c127df882620e2b0d
barbarabischain <bbischain@gmail.com>
2024-04-26 16:00:06 -0300

    props

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Card from "./components/Card/Card"
+import Contador from "./components/Contador/Contador"
+import Task from "./components/Task/Task"
+
+function App() {
+  return (
+    <>
+      {/* <Card titulo="React" descricao= "Biblioteca com JS" />
+
+      <Card titulo="Spring" descricao= "Framework de Java" /> */
+      
+      }
+      
+    {/* <Contador /> */}
+
+    <Task />
+    </>
+  )
+}
+
+export default App
